604da79b772b9e82ace3f9c84a9cd68898ac3e7b
namnguyen289 <vunam289@gmail.com>
2016-06-02 00:04:36 +0700

    improve filter table function

--- Source Code/app/pages/table/list-table/list-table.js
+++ Source Code/app/pages/table/list-table/list-table.js
@@ -60,17 +60,30 @@ export class TablePage {
          zone.tables = zone.tables.filter((val)=>{
           let isMatch = false;
           if(val.tbl_nm === undefined)return true;
-          if(val.tbl_nm != undefined && val.tbl_nm.toLowerCase().indexOf(this.query.toLowerCase()) > -1) return true;
-          let frstWrds = val.tbl_nm.split(' ');
+          let tbl_nm = this.bodauTiengViet(val.tbl_nm).toLowerCase();
+          let query = this.bodauTiengViet(this.query).toLowerCase();
+          if(tbl_nm != undefined && tbl_nm.indexOf(query) > -1) return true;
+          let frstWrds = tbl_nm.split(' ');
           let frstWrd = '';
           frstWrds.forEach(function(element) {
               frstWrd = frstWrd + '' + element.charAt(0);
           }, this);
-          if(frstWrd.toLowerCase().indexOf(this.query.toLowerCase()) > -1) return true;
+          if(frstWrd.indexOf(this.query) > -1) return true;
           
           return isMatch;
          });
          this.table[i] = zone;
       };
     }
+    bodauTiengViet(str) {
+        str = str.toLowerCase();
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+        str = str.replace(/đ/g, "d");
+        return str;
+}
 }
