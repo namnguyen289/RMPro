ef514ec9fa4da5d0040a6f7bda5e9dee5e890458
namnguyen289 <vunam289@gmail.com>
2016-06-12 12:46:40 +0700

    login by firebase

--- Source Code/app/pages/login/login.js
+++ Source Code/app/pages/login/login.js
@@ -1,40 +1,62 @@
-import {IonicApp, Page, NavController} from 'ionic-angular';
-import {TablePage} from '../table/list-table/list-table';
-import {SignupPage} from '../signup/signup';
-import {UserData} from '../../providers/user-data';
-import {FirebaseService} from '../../providers/firebaseService';
+import { IonicApp, Page, NavController } from 'ionic-angular';
+import { TablePage } from '../table/list-table/list-table';
+import { SignupPage } from '../signup/signup';
+import { UserData } from '../../providers/user-data';
+import { FirebaseService } from '../../providers/firebaseService';
+import { FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators, AbstractControl } from 'angular2/common';
 
 
 @Page({
-  templateUrl: 'build/pages/login/login.html'
+    templateUrl: 'build/pages/login/login.html',
+    directives: [FORM_DIRECTIVES]
 })
 export class LoginPage {
-  static get parameters() {
-    return [[NavController], [UserData],[FirebaseService]];
-  }
-
-  constructor(nav, userData,FBService) {
-    this.nav = nav;
-    this.userData = userData;
-    this.FBService = FBService;
-
-    this.login = {};
-    this.submitted = false;
-  }
-
-  onLogin(form) {
-    this.submitted = true;
-
-    if (form.valid) {
-      // this.userData.login(this.login.username,this.login.password);
-      this.FBService.login(this.login.username,this.login.password).subscribe((data)=>{
-           console.log("the data", data);
-           this.nav.setRoot(TablePage);
-         });
+    static get parameters() {
+        return [
+            [NavController],
+            [UserData],
+            [FirebaseService],
+            [FormBuilder]
+        ];
     }
+
+    constructor(nav, userData, FBService, formBuilder) {
+        this.nav = nav;
+        this.userData = userData;
+        this.FBService = FBService;
+        this.formBuilder = formBuilder;
+        this.login = {};
+        this.submitted = false;
+        this.authForm = formBuilder.group({
+            'email': ['', Validators.compose([Validators.required, this.checkEmailValidator])],
+            'password': ['', Validators.compose([Validators.required
+              // , Validators.minLength(8)
+              ])]
+        });
+
+        this.email = this.authForm.controls['email'];
+        this.password = this.authForm.controls['password'];
+    }
+
+    onSubmit(value) { 
+      this.submitted = true;
+
+        if (this.authForm.valid) {
+            // this.userData.login(this.login.username,this.login.password);
+            this.FBService.login(this.email.value, this.password.value).subscribe((data) => {
+                console.log("the data", data);
+                this.nav.setRoot(TablePage);
+            });
+        }
   }
 
-  onSignup() {
-    this.nav.push(SignupPage);
-  }
+    checkEmailValidator(control){
+      var regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!regexEmail.test(control.value)) {
+          return { checkEmailValidator: true };
+      }
+    }
+    onSignup() {
+        this.nav.push(SignupPage);
+    }
 }
